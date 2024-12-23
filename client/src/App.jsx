d2a10bf8aed55ef86e18a5fc31e26b20cc1923c4
David Ho <kietho.060193>
2024-12-23 09:20:21 +0700

    02 Setup Routes & Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard, Error, Landing, Login, Register } from "./pages";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/landing" element={<Landing />}></Route>
+        <Route path="*" element={<Error />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,7 @@
+import Dashboard from "./Dashboard";
+import Error from "./Error";
+import Landing from "./Landing";
+import Login from "./Login";
+import Register from "./Register";
+
+export { Dashboard, Landing, Login, Register, Error };
