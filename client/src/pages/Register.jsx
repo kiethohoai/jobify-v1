da01399b12187a39f32fdb460d75bdc430160f37
David Ho <kietho.060193>
2024-12-24 14:34:58 +0700

    07 Register Page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,51 @@
+import Wrapper from '../assets/wrappers/RegisterPage';
+import { useState } from 'react';
+import { Logo } from '../components';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+};
+
 const Register = () => {
+  const [values, setValues] = useState(initialState);
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return (
-    <div>Register</div>
-  )
-}
-export default Register
+    <Wrapper>
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        {/* name field */}
+        <div className="form-row">
+          <label className="form-label" htmlFor="name">
+            name
+          </label>
+          <input
+            className="form-input"
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={handleChange}
+          />
+        </div>
+
+        <button className="btn btn-block" type="submit">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
+};
+export default Register;
