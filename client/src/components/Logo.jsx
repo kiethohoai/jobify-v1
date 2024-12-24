ae83947ed5fe836fdb2e6728920b29c20cae230f
David Ho <kietho.060193>
2024-12-24 13:39:46 +0700

    05 Logo Component

---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,7 @@
+import logo from '../assets/image/logo.svg';
+
+const Logo = () => {
+  return <img className="logo" src={logo} alt="jobify logo" />;
+};
+
+export default Logo;
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,13 +1,13 @@
-import { Link } from 'react-router-dom';
-import logo from '../assets/image/logo.svg';
 import main from '../assets/image/main.svg';
+import { Link } from 'react-router-dom';
 import { Wrapper } from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img className="logo" src={logo} alt="jobify logo" />
+        <Logo />
       </nav>
 
       <div className="container page">
