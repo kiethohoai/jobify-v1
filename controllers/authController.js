8dcea8c74cb1b9eb71748985289912f9f1dfa991
David Ho <kietho.060193>
2024-12-26 16:00:25 +0700

    32 Hanle Duplicate Email

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,10 +4,19 @@ import { BadRequestError } from '../errors/index.js';
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
+
+  // chek empty fields
   if (!name || !email || !password) {
     throw new BadRequestError('please provide all values');
   }
 
+  // exists
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    throw new BadRequestError('Email already in use');
+  }
+
+  // create new one
   const user = await User.create({ name, email, password });
   res.status(StatusCodes.CREATED).json({ user });
 };
