03c9193334dd9aa5e70ca1d8bde9581eb055b312
David Ho <kietho.060193>
2024-12-26 10:20:54 +0700

    30 BadRequestError and NotFoundError (Handle Error)

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,14 +4,27 @@ import { StatusCodes } from 'http-status-codes';
 class CustomAPIError extends Error {
   constructor(message) {
     super(message);
+  }
+}
+
+class BadRequestError extends CustomAPIError {
+  constructor(message) {
+    super(message);
     this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
+
+class NotFoundError extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.NOT_FOUND;
   }
 }
 
 const register = async (req, res) => {
   const { name, email, password } = req.body;
   if (!name || !email || !password) {
-    throw new CustomAPIError('please provide all values');
+    throw new BadRequestError('please provide all values');
   }
 
   const user = await User.create({ name, email, password });
