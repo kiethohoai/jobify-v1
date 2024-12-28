698fb807717d07efdaf095cff5ef93d5d14fd2d8
David Ho <kietho.060193>
2024-12-28 14:13:11 +0700

    38 Login User Server

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(express.json());
-app.use(morgan('tiny'));
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+}
 
 // Routes
 app.get('/', (req, res) => {
