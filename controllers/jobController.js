68e531713dae717e0855bbc276d049c73220ef96
David Ho <kietho.060193>
2025-01-03 09:31:26 +0700

    52 Create Job

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,5 +1,20 @@
+import { BadRequestError, NotFoundError } from '../errors/index.js';
+import Job from '../models/Job.js';
+import { StatusCodes } from 'http-status-codes';
+
 const createJob = async (req, res) => {
-  res.send('createJob');
+  // guard
+  const { company, position } = req.body;
+  if (!company || !position) {
+    throw new BadRequestError('Please provide all values');
+  }
+
+  // create
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+
+  // send back res
+  res.status(StatusCodes.CREATED).json({ job });
 };
 
 const getAllJobs = async (req, res) => {
