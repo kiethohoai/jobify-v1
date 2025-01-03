456e22b5639e694060b3dc16081165915b0e9757
David Ho <kietho.060193>
2025-01-03 16:29:38 +0700

    54 Get All Job API

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -18,7 +18,12 @@ const createJob = async (req, res) => {
 };
 
 const getAllJobs = async (req, res) => {
-  res.send('getAllJobs');
+  const jobs = await Job.find({ createdBy: req.user.userId });
+  res.status(StatusCodes.OK).json({
+    jobs,
+    totalJob: jobs.length,
+    numOfPages: 1,
+  });
 };
 
 const updateJob = async (req, res) => {
