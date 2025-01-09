4b9c0db9707fc7f165c93ce617e7a203ed3ec35a
David Ho <kietho.060193>
2025-01-09 22:05:36 +0700

    62 Stats Server

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -2,6 +2,7 @@ import { BadRequestError, NotFoundError } from '../errors/index.js';
 import Job from '../models/Job.js';
 import { StatusCodes } from 'http-status-codes';
 import checkPermissions from '../utils/checkPermissions.js';
+import mongoose from 'mongoose';
 
 const createJob = async (req, res) => {
   // guard
@@ -76,7 +77,28 @@ const deleteJob = async (req, res) => {
 };
 
 const showStats = async (req, res) => {
-  res.send('showStats');
+  console.log(`🚀CHECK > req.user.userId:`, req.user.userId);
+
+  let stats = await Job.aggregate([
+    { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: '$status', count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, cur) => {
+    const { _id: title, count } = cur;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
+  let monthlyApplications = [];
+
+  res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
 
 export { createJob, deleteJob, getAllJobs, updateJob, showStats };
