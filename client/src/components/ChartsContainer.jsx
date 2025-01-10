70ea887ceb4a67ff645e7a04b46fa12fbba703af
David Ho <kietho.060193>
2025-01-10 10:20:49 +0700

    65 Monthly Applications FE

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+import { useAppContext } from '../context/appContext';
+import BarChartComponent from './BarsChart';
+import AreaChartComponent from './AreaChart';
+
 const ChartsContainer = () => {
+  const { monthlyApplications: data } = useAppContext();
+  const [barChart, setBarChart] = useState(false);
+
   return (
-    <div>
-      <h1>ChartsContainer</h1>
-    </div>
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+
+      {barChart ? <BarChartComponent data={data} /> : <AreaChartComponent data={data} />}
+    </Wrapper>
   );
 };
 export default ChartsContainer;
